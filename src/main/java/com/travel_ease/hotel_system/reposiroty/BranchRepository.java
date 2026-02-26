5ad3c6726cb42541f7006e0e3d8fc116545ee033
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-02-26 11:35:07 +0530

    feat(repository): create BranchRepository for Branch entity
    - Extends JpaRepository with Branch entity and String as ID type
    - Provides CRUD operations and JPA methods for Branch

---
+++ src/main/java/com/travel_ease/hotel_system/reposiroty/BranchRepository.java
@@ -0,0 +1,7 @@
+package com.travel_ease.hotel_system.reposiroty;
+
+import com.travel_ease.hotel_system.entity.Branch;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BranchRepository extends JpaRepository<Branch,String> {
+}

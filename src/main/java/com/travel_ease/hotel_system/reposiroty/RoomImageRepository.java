1a79fdaf977c4eb019a11158245c442247f423fe
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-02-26 11:36:27 +0530

    feat(repository): create RoomImageRepository for RoomImage entity
    - Extends JpaRepository with RoomImage entity and Long as ID type
    - Provides CRUD operations and JPA query support for RoomImage

---
+++ src/main/java/com/travel_ease/hotel_system/reposiroty/RoomImageRepository.java
@@ -0,0 +1,8 @@
+package com.travel_ease.hotel_system.reposiroty;
+
+import com.travel_ease.hotel_system.entity.RoomImage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoomImageRepository extends JpaRepository<RoomImage,Long> {
+
+}

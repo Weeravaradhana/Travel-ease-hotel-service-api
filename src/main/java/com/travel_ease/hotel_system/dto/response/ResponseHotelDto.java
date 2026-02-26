e1cfc3fbcf14e4ea0953208c8c9557e95583ecde
Weeravaradhana <thaminduweeravardana@gmail.com>
2026-02-26 14:22:58 +0530

    feat(dto): add ResponseHotelDto for hotel responses
    - Includes hotel details: `hotelId`, `hotelName`, `starRating`, `description`, `startingFrom`, `activeStatus`.
    - Tracks `createdAt` and `updatedAt`.
    - Contains list of `branches` as `ResponseBranchDto`.

---
+++ src/main/java/com/travel_ease/hotel_system/dto/response/ResponseHotelDto.java
@@ -0,0 +1,25 @@
+package com.travel_ease.hotel_system.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ResponseHotelDto {
+    private String hotelId;
+    private boolean activeStatus;
+    private LocalDateTime createdAt;
+    private LocalDateTime updatedAt;
+    private String description;
+    private String hotelName;
+    private int starRating;
+    private BigDecimal startingFrom;
+    private List<ResponseBranchDto> branches;
+}
